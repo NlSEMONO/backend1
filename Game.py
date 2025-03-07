645e181c13920dc349990e3aeb9f28ac22810877
Francis Madarang <fmadarang2542@hotmail.com>
2025-03-06 20:53:09 -0500

    Finished CLI version of game

--- Game.py
+++ Game.py
@@ -1,4 +1,6 @@
-from random import random
+import random
+
+type grid = list[list[bool]]
 
 class Game:
     R_BOUND = 8
@@ -7,6 +9,7 @@ class Game:
     _is_done = False
     true_symbol='#'
     false_symbol='.'
+    choices = [0, 0, 0]
     perms_2 = [
         [[True for _ in range(5)]], # 1x5
         [[True for _ in range(4)]], # 1x4
@@ -25,12 +28,13 @@ class Game:
         
     ]
     blocks = [
+        [[]], # padding
         [[True, True, True]] * 3, # 3x3
         [[True, True]] * 2, # 2x2
     ]
     all_blocks = 0
     
-    def _add_rotations(self, block: list[list[bool]], r):
+    def _add_rotations(self, block: grid, r: int):
         tmp = block
         self.blocks.append(block)
         for _ in range(r - 1):
@@ -52,17 +56,61 @@ class Game:
             
         self.all_blocks = len(self.blocks)
     
-    def print_matrix(self):
+    def _print_matrix(self):
+        c = 0
         for row in self.matrix:
-            print(' '.join(self.true_symbol if cell else self.false_symbol for cell in row))
+            print(f"{c + 1} " + ' '.join(self.true_symbol if cell else self.false_symbol for cell in row))
+            c += 1
+        print(" ", end="")
+        for i in range(self.R_BOUND):
+            print(f" {i + 1}", end="")
+        print()
     
-    def print_blocks(self):
-        print(self.blocks)
+    def _print_line(self):
+        print()
+    
+    def _print_blocks(self):
+        m = max([len(self.blocks[c]) for c in self.choices])
+        size = 6
+        for i in range(m):
+            print_str = ""
+            for c in self.choices:
+                if len(self.blocks[c]) > i: 
+                    width = len(self.blocks[c][0])
+                    buf = size - width
+                    print_str += " " * (buf // 2)
+                    for j in range(width):
+                        print_str += self.true_symbol if self.blocks[c][i][j] else self.false_symbol
+                    print_str += " " * ((buf // 2) + (buf % 2)) + "|"
+                else:
+                    print_str += " " * (size) + "|"
+            print(print_str)
+        print_str = ""
+        for i in range(len(self.choices)):
+            buf = size
+            print_str += " " * (buf // 2) + f"{i + 1}" + " " * ((buf // 2) + (buf % 2))
+        print(print_str)
     
     def is_done(self):
         return self._is_done
     
-    def _validate_action(self, b, x, y, matrix, print_error = True):
+    def _remove_block(self, b: int, x: int, y: int, matrix: grid):
+        n = len(self.blocks[b])
+        m = len(self.blocks[b][0])
+        for i in range(n):
+            for j in range(m): 
+                if self.blocks[b][i][j]:
+                    matrix[i+x][j+y] = False
+    
+    def _place_block(self, b: int, x: int, y: int, matrix: grid):
+        n = len(self.blocks[b])
+        m = len(self.blocks[b][0])
+        for i in range(n):
+            for j in range(m):
+                if self.blocks[b][i][j]:
+                    matrix[i+x][j+y] = True
+    
+    def _validate_action(self, b: int, x: int, y: int, matrix: grid, print_error = True):
         if x < self.L_BOUND or x >= self.R_BOUND or y < self.L_BOUND or y >= self.R_BOUND:
             if (print_error):
                 print("Invalid coordinates.")
@@ -84,24 +132,7 @@ class Game:
                 
         return True
     
-    def _remove_block(self, b, x, y, matrix):
-        n = len(self.blocks[b])
-        m = len(self.blocks[b][0])
-        for i in range(n):
-            for j in range(m): 
-                if self.blocks[b][i][j]:
-                    matrix[i+x][j+y] = False
-    
-    def _place_block(self, b, x, y, matrix):
-        n = len(self.blocks[b])
-        m = len(self.blocks[b][0])
-        for i in range(n):
-            for j in range(m):
-                if self.blocks[b][i][j]:
-                    matrix[i+x][j+y] = True
-                    # self.print_matrix()
-    
-    def _remove_rows_and_cols(self, matrix):
+    def _remove_rows_and_cols(self, matrix: grid):
         # track rows and cols removed
         r = []
         c = []
@@ -123,21 +154,26 @@ class Game:
                 
         return [r, c]
     
-    def _add_rows_and_cols(self, r, c, m):
+    def _add_rows_and_cols(self, r: int, c: int , m: grid):
         for row in r:
             for i in range(self.R_BOUND):
                 m[row][i] = True
         for col in c:
             for i in range(self.R_BOUND):
                 m[i][col] = True
-                
-    def _next_3_wkr_gen(self, res, arr, m, idx):
+    
+    def _next_3_wkr_gen(self, res, arr, m, idx, dp):
+        # see if there is success case
         if len(arr) == 3:
             res.add(tuple(arr))
-            return True
+            dp[arr[0]][arr[1]][arr[2]] = True
         else:
-            live = False
             for k in range(idx, self.all_blocks):
+                # don't compute if success for 3 chosen blocks alr exists
+                if len(arr) == 2 and dp[arr[0]][arr[1]][k]:
+                    continue
+                
+                # compute all possible locations of next block
                 for i in range(self.R_BOUND):
                     for j in range(self.R_BOUND):
                         # if placing block on valid index, see if it is possible to form a valid group of 3 w/o death
@@ -145,28 +181,41 @@ class Game:
                             self._place_block(k, i, j, m)
                             r, c = self._remove_rows_and_cols(m)
                             arr.append(k)
-                            rr = self._next_3_wkr_gen(res, arr, m, k)
+                            rr = self._next_3_wkr_gen(res, arr, m, k, dp)
                             arr.pop()
                             self._add_rows_and_cols(r, c, m)
                             self._remove_block(k, i, j, m)
-                            live = rr or live
                             
                             # jump to next block if current block has valid group of 3
                             if rr:
-                                with open("try_log.log", 'a') as f:
-                                    f.write(f'{i * 8 + j} tries')
                                 i = self.R_BOUND
                                 j = self.R_BOUND
-            return live
-                    
+        
     def _get_next_3(self):
         res = set()
+        dp = [[[False for _ in range(self.all_blocks)] for _ in range(self.all_blocks)] for _ in range(self.all_blocks)]
         recur_matrix = [[self.matrix[i][j] for j in range(self.R_BOUND)] for i in range(self.R_BOUND)]
-        self._next_3_wkr_gen(res, [], recur_matrix, 0)
-        random.choice(res)
+        self._next_3_wkr_gen(res, [], recur_matrix, 1, dp)
+        choice = random.choice(list(res))
+        self.choices = list(choice)
         
     def perform_action(self, b, x, y):
-        if self._validate_action(b, x, y, self.matrix):
-            self._place_block(b, x, y, self.matrix)
-            self._remove_rows_and_cols()
+        b -= 1
+        x -= 1
+        y -= 1
+        if b >= 3 or b < 0:
+            print("Invalid block selected")
+            return
         
+        if self._validate_action(self.choices[b], x, y, self.matrix):
+            self._place_block(self.choices[b], x, y, self.matrix)
+            self.choices[b] = 0
+            self._remove_rows_and_cols(self.matrix)
+            
+    
+    def print_state(self):
+        if all([c == 0 for c in self.choices]):
+            self._get_next_3()
+        self._print_matrix()
+        self._print_line()
+        self._print_blocks()
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from Game import Game
 if __name__ == '__main__':
     g = Game()
-    # print(g.blocks)
+    g.print_state()
     while not g.is_done():
-        g._get_next_3()
         action = [int(x) for x in list(input().split())]
         g.perform_action(action[0], action[1], action[2])
-        g.print_matrix()
+        g.print_state()
     g.print_blocks()
